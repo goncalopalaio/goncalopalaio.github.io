6d92d5f8daf8f1ad840cc4b087db237154322c32
Goncalo Palaio <goncalo.oliveira@wit-software.com>
2019-09-02 17:08:26 +0100

    clean.sh

--- generate_and_commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Stop deployment if a command fails
-set -e 
-
-
---
+++ hugo/clean.sh
@@ -0,0 +1,13 @@
+rm ../404.html
+rm -rf ../fonts
+rm -rf ../images
+rm -rf ../js
+rm -rf ../resources
+rm -rf ../categories
+rm ../index.html
+rm -rf ../page
+rm ../sitemap.xml
+rm -rf ../css
+rm ../index.xml
+rm -rf ../posts
+rm -rf ../tags
